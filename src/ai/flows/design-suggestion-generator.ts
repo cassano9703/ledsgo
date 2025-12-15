fb6ab5c5c2e844f41969217563b933971a61395c
carlos cassano <cassano9703@gmail.com>
2025-12-15 21:36:49 +0000

    persiste el error, tal ves el error puede estar en la conectividad con e

--- src/ai/flows/design-suggestion-generator.ts
+++ src/ai/flows/design-suggestion-generator.ts
@@ -34,9 +34,22 @@ const prompt = ai.definePrompt({
   input: {schema: DesignSuggestionInputSchema},
   output: {schema: DesignSuggestionOutputSchema},
   config: {
+    model: 'googleai/gemini-2.5-flash',
     safetySettings: [
       {
         category: 'HARM_CATEGORY_DANGEROUS_CONTENT',
+        threshold: 'BLOCK_NONE',
+      },
+       {
+        category: 'HARM_CATEGORY_HATE_SPEECH',
+        threshold: 'BLOCK_NONE',
+      },
+      {
+        category: 'HARM_CATEGORY_HARASSMENT',
+        threshold: 'BLOCK_NONE',
+      },
+      {
+        category: 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
         threshold: 'BLOCK_NONE',
       },
     ],
