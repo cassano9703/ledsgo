04d2d415c9d3be8ff1d987d8e8d8dcfbe8889716
carlos cassano <cassano9703@gmail.com>
2025-12-17 16:54:39 +0000

    esta bien el tamaño de la pantalla porque visualizo mayor espacio a lado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <Header />
       <main className="flex-1">
         <section id="configurator" className="py-12 md:py-20 lg:py-24">
-          <div className="container px-4 md:px-6">
+          <div className="w-full px-4 md:px-6">
             <div className="flex flex-col items-center space-y-4 text-center">
               <div className="space-y-2">
                 <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl font-headline">
