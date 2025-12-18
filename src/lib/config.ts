ab6e2dc3e558a1e7c09684d01e095546d5fb9ba0
carlos cassano <cassano9703@gmail.com>
2025-12-18 19:38:49 +0000

    corregir error

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -53,9 +53,9 @@ export type BackgroundConfig = {
 };
 
 export const backgrounds: BackgroundConfig[] = [
-    { name: "Grass Sintético", imageUrl: "https://images.unsplash.com/photo-1599419799298-f10557053530?q=80&w=2070&auto=format&fit=crop" },
-    { name: "Panel de Madera", imageUrl: "https://images.unsplash.com/photo-1616047006789-b7af5afb8c20?q=80&w=2070&auto=format&fit=crop" },
-    { name: "Pared de Ladrillo", imageUrl: "https://images.unsplash.com/photo-1561053258-ed994eea349a?q=80&w=2070&auto=format&fit=crop" },
-    { name: "Pared Blanca de Salón", imageUrl: "https://images.unsplash.com/photo-1554995207-c18c203602cb?q=80&w=2070&auto=format&fit=crop" },
-    { name: "Pared Oscura", imageUrl: "https://images.unsplash.com/photo-1619252584172-a83a949b6efd?q=80&w=1887&auto=format&fit=crop" },
+    { name: "Grass Sintético", imageUrl: "https://i.imgur.com/sI68Yek.jpeg" },
+    { name: "Panel de Madera", imageUrl: "https://i.imgur.com/aAelWay.jpeg" },
+    { name: "Pared Cementada", imageUrl: "https://i.imgur.com/53KZ8PC.jpeg" },
+    { name: "Pared Oscura", imageUrl: "https://i.imgur.com/FacpyFd.jpeg" },
+    { name: "Pared Gris", imageUrl: "https://images.unsplash.com/photo-1619252584172-a83a949b6efd?q=80&w=1887&auto=format&fit=crop" },
 ];
