a5b9f6e5888dac9400f25fac5b5949bcfe9459b9
carlos cassano <cassano9703@gmail.com>
2026-01-19 15:39:46 +0000

    el tamaño es muy grande, reducirlo por favor

--- src/app/crear/page.tsx
+++ src/app/crear/page.tsx
@@ -22,9 +22,12 @@ export default function CreatePage() {
           </div>
         </section>
       </main>
+
+
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-muted-foreground">&copy; {new Date().getFullYear()} Leds Go. Todos los derechos reservados.</p>
       </footer>
     </div>
+
   );
 }
