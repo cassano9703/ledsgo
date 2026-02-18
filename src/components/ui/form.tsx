4d4c85b08d8a4855b404043279f2f28214416fbc
carlos cassano <cassano9703@gmail.com>
2026-02-18 19:26:57 +0000

    verificar error

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -97,6 +97,7 @@ const FormLabel = React.forwardRef<
       ref={ref}
       className={cn(error && "text-destructive", className)}
       htmlFor={formItemId}
+      suppressHydrationWarning
       {...props}
     />
   )
@@ -119,6 +120,7 @@ const FormControl = React.forwardRef<
           : `${formDescriptionId} ${formMessageId}`
       }
       aria-invalid={!!error}
+      suppressHydrationWarning
       {...props}
     />
   )
